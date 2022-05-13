bec5a3f60ffb4bc753974b4dae4321db46729052
Christian <Christian@DESKTOP-NBDTTBA>
2022-05-13 10:04:23 +0700

    Add project files.

---
+++ Materi13Mei/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Materi13Mei
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tb_IDPelajar = new System.Windows.Forms.TextBox();
+            this.tb_NamaPelajar = new System.Windows.Forms.TextBox();
+            this.cb_Jurusan = new System.Windows.Forms.ComboBox();
+            this.dtp_TanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.tb_NoInduk = new System.Windows.Forms.TextBox();
+            this.tb_NoTelepon = new System.Windows.Forms.TextBox();
+            this.rb_LakiLaki = new System.Windows.Forms.RadioButton();
+            this.rb_Perempuan = new System.Windows.Forms.RadioButton();
+            this.tb_Alamat = new System.Windows.Forms.TextBox();
+            this.cb_Angkatan = new System.Windows.Forms.ComboBox();
+            this.tb_EMail = new System.Windows.Forms.TextBox();
+            this.bt_Insert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Pelajar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No Induk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jurusan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No Telepon";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Alamat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jenis Kelamin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Angkatan";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "E-Mail";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 107);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Tanggal Lahir";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(20, 51);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Nama Pelajar";
+            // 
+            // tb_IDPelajar
+            // 
+            this.tb_IDPelajar.Location = new System.Drawing.Point(127, 22);
+            this.tb_IDPelajar.Name = "tb_IDPelajar";
+            this.tb_IDPelajar.Size = new System.Drawing.Size(100, 20);
+            this.tb_IDPelajar.TabIndex = 11;
+            // 
+            // tb_NamaPelajar
+            // 
+            this.tb_NamaPelajar.Location = new System.Drawing.Point(127, 48);
+            this.tb_NamaPelajar.Name = "tb_NamaPelajar";
+            this.tb_NamaPelajar.Size = new System.Drawing.Size(100, 20);
+            this.tb_NamaPelajar.TabIndex = 12;
+            // 
+            // cb_Jurusan
+            // 
+            this.cb_Jurusan.FormattingEnabled = true;
+            this.cb_Jurusan.Location = new System.Drawing.Point(127, 74);
+            this.cb_Jurusan.Name = "cb_Jurusan";
+            this.cb_Jurusan.Size = new System.Drawing.Size(121, 21);
+            this.cb_Jurusan.TabIndex = 13;
+            // 
+            // dtp_TanggalLahir
+            // 
+            this.dtp_TanggalLahir.Location = new System.Drawing.Point(127, 101);
+            this.dtp_TanggalLahir.Name = "dtp_TanggalLahir";
+            this.dtp_TanggalLahir.Size = new System.Drawing.Size(200, 20);
+            this.dtp_TanggalLahir.TabIndex = 14;
+            // 
+            // tb_NoInduk
+            // 
+            this.tb_NoInduk.Location = new System.Drawing.Point(127, 127);
+            this.tb_NoInduk.Name = "tb_NoInduk";
+            this.tb_NoInduk.Size = new System.Drawing.Size(100, 20);
+            this.tb_NoInduk.TabIndex = 15;
+            // 
+            // tb_NoTelepon
+            // 
+            this.tb_NoTelepon.Location = new System.Drawing.Point(127, 153);
+            this.tb_NoTelepon.Name = "tb_NoTelepon";
+            this.tb_NoTelepon.Size = new System.Drawing.Size(100, 20);
+            this.tb_NoTelepon.TabIndex = 16;
+            // 
+            // rb_LakiLaki
+            // 
+            this.rb_LakiLaki.AutoSize = true;
+            this.rb_LakiLaki.Location = new System.Drawing.Point(127, 182);
+            this.rb_LakiLaki.Name = "rb_LakiLaki";
+            this.rb_LakiLaki.Size = new System.Drawing.Size(74, 17);
+            this.rb_LakiLaki.TabIndex = 17;
+            this.rb_LakiLaki.TabStop = true;
+            this.rb_LakiLaki.Text = "Laki - Laki";
+            this.rb_LakiLaki.UseVisualStyleBackColor = true;
+            // 
+            // rb_Perempuan
+            // 
+            this.rb_Perempuan.AutoSize = true;
+            this.rb_Perempuan.Location = new System.Drawing.Point(218, 182);
+            this.rb_Perempuan.Name = "rb_Perempuan";
+            this.rb_Perempuan.Size = new System.Drawing.Size(79, 17);
+            this.rb_Perempuan.TabIndex = 18;
+            this.rb_Perempuan.TabStop = true;
+            this.rb_Perempuan.Text = "Perempuan";
+            this.rb_Perempuan.UseVisualStyleBackColor = true;
+            // 
+            // tb_Alamat
+            // 
+            this.tb_Alamat.Location = new System.Drawing.Point(127, 205);
+            this.tb_Alamat.Name = "tb_Alamat";
+            this.tb_Alamat.Size = new System.Drawing.Size(100, 20);
+            this.tb_Alamat.TabIndex = 19;
+            // 
+            // cb_Angkatan
+            // 
+            this.cb_Angkatan.FormattingEnabled = true;
+            this.cb_Angkatan.Location = new System.Drawing.Point(127, 231);
+            this.cb_Angkatan.Name = "cb_Angkatan";
+            this.cb_Angkatan.Size = new System.Drawing.Size(121, 21);
+            this.cb_Angkatan.TabIndex = 20;
+            // 
+            // tb_EMail
+            // 
+            this.tb_EMail.Location = new System.Drawing.Point(127, 258);
+            this.tb_EMail.Name = "tb_EMail";
+            this.tb_EMail.Size = new System.Drawing.Size(100, 20);
+            this.tb_EMail.TabIndex = 21;
+            // 
+            // bt_Insert
+            // 
+            this.bt_Insert.Location = new System.Drawing.Point(23, 292);
+            this.bt_Insert.Name = "bt_Insert";
+            this.bt_Insert.Size = new System.Drawing.Size(75, 23);
+            this.bt_Insert.TabIndex = 22;
+            this.bt_Insert.Text = "Insert";
+            this.bt_Insert.UseVisualStyleBackColor = true;
+            this.bt_Insert.Click += new System.EventHandler(this.bt_Insert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_Insert);
+            this.Controls.Add(this.tb_EMail);
+            this.Controls.Add(this.cb_Angkatan);
+            this.Controls.Add(this.tb_Alamat);
+            this.Controls.Add(this.rb_Perempuan);
+            this.Controls.Add(this.rb_LakiLaki);
+            this.Controls.Add(this.tb_NoTelepon);
+            this.Controls.Add(this.tb_NoInduk);
+            this.Controls.Add(this.dtp_TanggalLahir);
+            this.Controls.Add(this.cb_Jurusan);
+            this.Controls.Add(this.tb_NamaPelajar);
+            this.Controls.Add(this.tb_IDPelajar);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tb_IDPelajar;
+        private System.Windows.Forms.TextBox tb_NamaPelajar;
+        private System.Windows.Forms.ComboBox cb_Jurusan;
+        private System.Windows.Forms.DateTimePicker dtp_TanggalLahir;
+        private System.Windows.Forms.TextBox tb_NoInduk;
+        private System.Windows.Forms.TextBox tb_NoTelepon;
+        private System.Windows.Forms.RadioButton rb_LakiLaki;
+        private System.Windows.Forms.RadioButton rb_Perempuan;
+        private System.Windows.Forms.TextBox tb_Alamat;
+        private System.Windows.Forms.ComboBox cb_Angkatan;
+        private System.Windows.Forms.TextBox tb_EMail;
+        private System.Windows.Forms.Button bt_Insert;
+    }
+}
+
---
+++ Materi13Mei/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Materi13Mei
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection sqlConnect = new MySqlConnection("server=localhost;uid=root;pwd=;database=premier_league");
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        String sqlQuery;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void bt_Insert_Click(object sender, EventArgs e)
+        {
+            sqlQuery = "INSERT INTO pelajar VALUES('IDPelajar', 'NamaPelajar', 'Jurusan', 'TanggalLahir', 'NoInduk', 'NoTelepon', 'JenisKelamin', 'Alamat', 'Angkatan', 'Email', 0)";
+            MessageBox.Show(sqlQuery);
+        }
+    }
+}
